de066370b24911988d8e4ba6d668af6454bb3374
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-01-13 09:57:56 +0530

    replaced class with className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ class Footer extends React.Component {
     render() {
         return (
             <footer id="footer">
-                <div class="footer-top">
-                    <div class="container">
-                        <div class="row">
-                            <div class="footer-contact">
+                <div className="footer-top">
+                    <div className="container">
+                        <div className="row">
+                            <div className="footer-contact">
                                 <h4 style={{ fontSize: 19 }}>TechMarathon'<a style={{fontSize: "large", color: "#00ff41"}}>20</a></h4>
                                 <p> TechMarathon is the Annual Technical fest hosted by Department of Computer Science - "Sanganika", Deen Dayal Upadhyaya College, University of Delhi. Over the course of 9 years since it's inception, Tech Marathon has grown leaps and bounds and has now become a mature and respected event by the participation from highly reputed colleges of Delhi NCR and sponsors such as HP, Zebronics, codechef and many others.
                                     <br />
@@ -16,14 +16,14 @@ class Footer extends React.Component {
                                     <br /> <strong>Email:</strong> <a href="mailto:dduc.techmarathon@gmail.com">dduc.techmarathon@gmail.com</a>
                                     <br />
                                 </p>
-                                <div class="social-links"> <a href="https://twitter.com/dduc_tm" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a> <a href="https://m.facebook.com/techmarathon/?__tn__=C-R" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a> <a href="https://instagram.com/sanganika_dduc" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a> </div>
+                                <div className="social-links"> <a href="https://twitter.com/dduc_tm" target="_blank" className="twitter"><i className="fa fa-twitter"></i></a> <a href="https://m.facebook.com/techmarathon/?__tn__=C-R" target="_blank" className="facebook"><i className="fa fa-facebook"></i></a> <a href="https://instagram.com/sanganika_dduc" target="_blank" className="instagram"><i className="fa fa-instagram"></i></a> </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="container">
-                    <div class="copyright"> &copy; Copyright <strong>Sanganika</strong>. All Rights Reserved </div>
-                    <div class="credits"> Made by <a href="https://www.linkedin.com/in/ankit-kumar-4b6714161" target="_blank">Ankit Kumar</a> </div>
+                <div className="container">
+                    <div className="copyright"> &copy; Copyright <strong>Sanganika</strong>. All Rights Reserved </div>
+                    <div className="credits"> Made by <a href="https://www.linkedin.com/in/ankit-kumar-4b6714161" target="_blank">Ankit Kumar</a> </div>
                 </div>
             </footer>
         );
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,11 +7,11 @@ class Intro extends React.Component {
 
                 <canvas></canvas>
 
-                <div class="intro-container wow fadeIn">
-                    <h1 class="mb-4 pb-0">The Annual<br/><span>Sanganika</span> Fest</h1>
-                    <p class="mb-4 pb-0">6 March, DDU College, Dwarka, New Delhi</p>
-                    <p class="mb-4 pb-0"><a href="https://goo.gl/maps/57JA6C76gQU8xQzeA" target= " "><img width="13px" src="img/map.webp"/> 28°36'18.6"N 77°02'16.9"E</a></p>
-                    <a href="https://youtu.be/qCeFEy1hk8s" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a> <a href="http://portal.techmarathon.in/auth/signUp" target="_blank" class="about-btn">Register</a>
+                <div className="intro-container wow fadeIn">
+                    <h1 className="mb-4 pb-0">The Annual<br/><span>Sanganika</span> Fest</h1>
+                    <p className="mb-4 pb-0">6 March, DDU College, Dwarka, New Delhi</p>
+                    <p className="mb-4 pb-0"><a href="https://goo.gl/maps/57JA6C76gQU8xQzeA" target= " "><img width="13px" src="img/map.webp"/> 28°36'18.6"N 77°02'16.9"E</a></p>
+                    <a href="https://youtu.be/qCeFEy1hk8s" className="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a> <a href="http://portal.techmarathon.in/auth/signUp" target="_blank" className="about-btn">Register</a>
                 </div>
                 <script src="js/custom.js"></script>
             </section>
